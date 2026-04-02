273977329d13fed6911ea339888d437be3fda853
samarthyaarathod-cell <samarthya.a.rathod@gmail.com>
2026-04-02 19:10:30 +0530

    Add files via upload

---
+++ Maximum_Subarray.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            currentSum = max(nums[i], currentSum + nums[i]);
+            maxSum = max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+};
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    Solution obj;
+    int result = obj.maxSubArray(nums);
+
+    cout << "Maximum Subarray Sum: " << result << endl;
+
+    return 0;
+}
